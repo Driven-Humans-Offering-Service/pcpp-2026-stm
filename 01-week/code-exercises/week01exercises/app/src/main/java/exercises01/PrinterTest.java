180b7b7cf2d2d52e8b149efcd53d041c712fff2d
iiinaD <daniel@haugboelle.dk>
2026-08-24 11:34:14 +0200

    exercise 1.2

--- 01-week/code-exercises/week01exercises/app/src/main/java/exercises01/PrinterTest.java
+++ 01-week/code-exercises/week01exercises/app/src/main/java/exercises01/PrinterTest.java
@@ -1,11 +1,39 @@
 package exercises01;
 
-class PrinterTest {
-    class Printer {
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class PrinterTest {
+
+    public static void main(String[] args) {
+        Printer p = new Printer();
+
+        Thread t1 = new Thread(() -> {
+            for (;;) {
+                p.print();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (;;) {
+                p.print();
+            }
+        });
+        t1.start(); t2.start();
+        try { t1.join(); t2.join(); }
+        catch (InterruptedException exn) {
+            System.out.println("Some thread was interrupted");
+        }
+    }
+
+    static class Printer {
+        Lock l = new ReentrantLock();
+
         public void print() {
+            l.lock();
             System.out.print("-");
             try { Thread.sleep(50); } catch (InterruptedException exn) { }
             System.out.print("|");
+            l.unlock();
         }
     }
 }
